a017d90a0b8efd44d6570433277d0c6c7ffd85b3
MiguelRRJose <00095322@uca.edu.sv>
2025-06-08 16:59:18 -0600

    :construction:Implementación de la navegación

---
+++ app/src/main/java/com/pdmproyecto/mymedicine/ui/screens/Navigation/AppNavigation.kt
@@ -0,0 +1,16 @@
+package com.pdmproyecto.mymedicine.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.pdmproyecto.mymedicine.ui.screens.Login.LoginPrincipal.LoginScreen
+import com.pdmproyecto.mymedicine.ui.screens.PatientDashboard.PatientDashboardScreen
+
+@Composable
+fun AppNavigation(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = "login") {
+        composable("login") { LoginScreen(navController) }
+        composable("dashboard") { PatientDashboardScreen() }
+    }
+}
