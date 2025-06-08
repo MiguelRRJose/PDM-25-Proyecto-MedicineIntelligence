c786bd1752bf6b4c09d74149c164f592ca4fa342
MiguelRRJose <00095322@uca.edu.sv>
2025-06-08 17:00:47 -0600

    :construction:Pantalla de registro y pantalla inicial para el usuario completa

--- app/src/main/java/com/pdmproyecto/mymedicine/MainActivity.kt
+++ app/src/main/java/com/pdmproyecto/mymedicine/MainActivity.kt
@@ -5,13 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.pdmproyecto.mymedicine.ui.screens.Login.LoginPrincipal.LoginScreen
 import com.pdmproyecto.mymedicine.ui.theme.MyMedicineTheme
+import androidx.navigation.compose.rememberNavController
+import com.pdmproyecto.mymedicine.ui.navigation.AppNavigation
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +18,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyMedicineTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val navController = rememberNavController()
+                AppNavigation(navController)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyMedicineTheme {
-        Greeting("Android")
     }
 }
