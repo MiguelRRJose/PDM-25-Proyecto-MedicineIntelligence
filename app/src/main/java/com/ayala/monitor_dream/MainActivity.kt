ac55a50638719ce53e248caa4dbdaed133d9d116
NesjsjsjsjsjsA <00133723@uca.edu.sv>
2025-07-03 09:13:36 -0600

    DONE

--- app/src/main/java/com/ayala/monitor_dream/MainActivity.kt
+++ app/src/main/java/com/ayala/monitor_dream/MainActivity.kt
@@ -1,20 +1,66 @@
 package com.ayala.monitor_dream
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
 import androidx.navigation.compose.rememberNavController
+import com.ayala.monitor_dream.navigation.AlarmP
 import com.ayala.monitor_dream.navigation.AppNavigator
+import com.ayala.monitor_dream.navigation.SleepY
 
 
 class MainActivity : ComponentActivity() {
 
+    private val navigationTarget = mutableStateOf<Any?>(null)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        handleIntent(intent)
+
         setContent {
 
             val navController = rememberNavController()
+
             AppNavigator(navController)
+
+            val target = navigationTarget.value
+
+            LaunchedEffect(target){
+                if (target!=null) {
+
+                    Log.d("Notificacion!!", "Notificación recibida")
+                    try {
+                        navController.navigate(target)
+                    } catch (e: IllegalArgumentException) {
+                            Log.d("Notificacion!!!!", "Notificación recibida")
+                        }
+
+                    navigationTarget.value = null
+
+                }
+            }
+        }
+    }
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        if(true){
+        handleIntent(intent)}
+    }
+    private fun handleIntent(intent: Intent) {
+
+        val routeExtra ="NAVIGATION_TO_SLEEP_Y_SCREEN"
+
+        if (intent.hasExtra(routeExtra)) {
+
+            if (intent.getBooleanExtra(routeExtra, false)){
+                navigationTarget.value = AlarmP
+
+            }
         }
     }
 }
--- app/src/main/java/com/ayala/monitor_dream/notify/NotificationReceiver.kt
+++ app/src/main/java/com/ayala/monitor_dream/notify/NotificationReceiver.kt
@@ -1,27 +1,51 @@
 package com.ayala.monitor_dream.notify
 
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import com.ayala.monitor_dream.MainActivity
+import com.ayala.monitor_dream.navigation.SleepDetail
+import com.ayala.monitor_dream.navigation.SleepY
 
 class NotificationReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent?)
     {
         Log.d("Notification!!", "Notificación recibida")
 
+        val contentIntent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            putExtra("NAVIGATION_TO_SLEEP_Y_SCREEN", true)
+        }
+
+        val pendingIntentFlags = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        } else {
+            PendingIntent.FLAG_UPDATE_CURRENT
+        }
+
+        val contentPendingIntent: PendingIntent = PendingIntent.getActivity(
+            context,
+            1,
+            contentIntent,
+            pendingIntentFlags
+        )
+
         try {
             val builder = NotificationCompat.Builder(context, "dream_channel_id")
-                .setSmallIcon(com.ayala.monitor_dream.R.drawable.moon_purple_2)
+                .setSmallIcon(com.ayala.monitor_dream.R.drawable.astro_durmiendo)
                 .setContentTitle("SleepY te ha mandado una notificación!!!")
                 .setContentText("¡Es hora de dormir!")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setAutoCancel(true)
+                .setContentIntent(contentPendingIntent)
 
             val manager = NotificationManagerCompat.from(context)
             manager.notify(1001, builder.build())
-            Log.d("Notification", "Notificación enviada")
         } catch (e: SecurityException) {
             Log.e("Notification", "Permiso no otorgado: ${e.message}")
         }
